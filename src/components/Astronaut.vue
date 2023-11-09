54862cb9360be7218ab8c355f1f19ae8bd1483b5
Dennis Rodriguez <dennisrodriguezx@gmail.com>
2023-11-09 01:59:16 -0400

    INIT

---
+++ src/components/Astronaut.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup () {
+        
+
+        return {}
+    }
+})
+</script>
+<template>
+    <div>
+
+    </div>
+</template>
---
+++ src/components/Counter..vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps<{
+    initialCount: number
+}>()
+
+const counter = ref<number>(props.initialCount ?? 0)
+</script> 
+<template>
+    <slot name="top"></slot>
+    <button @click="counter++">{{ counter }}</button>
+    <slot name="bottom"></slot>
+</template>
