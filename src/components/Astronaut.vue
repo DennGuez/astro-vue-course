fa35a2d9e66ff99974293ee81525f3a0e711bd94
Dennis Rodriguez <dennisrodriguezx@gmail.com>
2023-11-09 18:07:34 -0400

    Sprinkle in an Interactive Header on the Home Page

--- src/components/Astronaut.vue
+++ src/components/Astronaut.vue
@@ -1,16 +1,31 @@
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import { useMouse } from '@vueuse/core'
 
-export default defineComponent({
-    setup () {
-        
+const { x, y } = useMouse();
+const el = ref()
+const cssRotate = computed(() => {
+    if (!el.value) return
+    const rect = el.value.getBoundingClientRect()
+    const centerX = rect.left + rect.width / 2
+    const centerY = rect.top + rect.height / 2
 
-        return {}
-    }
+    const deltaX = x.value - centerX
+    const deltaY = y.value - centerY
+
+    const rad = Math.atan2(deltaY, deltaX)
+    const deg = rad * (180 / Math.PI) + 90
+
+    return `rotate(${deg}deg)`
 })
 </script>
 <template>
-    <div>
-
-    </div>
+    <span ref="el" id="rotateMe">🧑‍🚀</span>
 </template>
+<style scoped>
+#rotateMe {
+    transform-origin: center center;
+    transform: v-bind(cssRotate);
+    display: inline-block;
+}
+</style>
